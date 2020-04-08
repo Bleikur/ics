00a36c449ce0f9a8c28fdc5a7504aaecc708a381
Bleikur <bleikurhestur@gmail.com>
2020-04-08 17:19:48 -0500

    2.0
    proj part 2

--- searchItem.php
+++ searchItem.php
@@ -1,10 +1,34 @@
 <!DOCTYPE HTML>
+
 <html>
-this page will return entries in the database that match your query:
+  <script
+		src="https://code.jquery.com/jquery-3.3.1.js"
+		integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60="
+		crossorigin="anonymous">
+	</script>
+  <script> 
+    $(function(){
+      $("#header").load("header.html"); 
+      $("#footer").load("footer.html"); 
+    });
+  </script>
 
-<?php
-	$query = $_GET["search"];
+  <link rel="stylesheet" type="text/css" href="style.css"/>
+
+  <head>
+    <title>ReChord</title>
+  </head>
+
+  <div id="header"></div>
+
+    <body>
+		<p>this page will return entries in the database that match your query:</p>
+    </body>
+
+  <div id="footer"></div>
+
+  <?php
+	$query = $_GET["searchText"];
 	echo $query;
-?>
-
+  ?>
 </html>
